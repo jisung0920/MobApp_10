ee7c22975f0a5d2ffc5e1fe56af6dbf08ff4f6a8
jisung <jisung0920@gmail.com>
2017-05-04 13:30:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jisung/mobapp_10/S_Activity.java
@@ -0,0 +1,13 @@
+package com.example.jisung.mobapp_10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class S_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_);
+    }
+}
